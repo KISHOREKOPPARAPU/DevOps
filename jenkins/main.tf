b637e4532ec46750090381121b93290481602690
KISHORE KOPPARAPU <114247285+KISHOREKOPPARAPU@users.noreply.github.com>
2024-04-30 18:53:40 +0530

    Update main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,47 +1,10 @@
 provider "aws" {
 region = "us-east-1"
 }
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 8080 and 22"
-
-  # allow access on port 8080
-  ingress {
-    description = "http proxy access"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # allow access on port 22
-  ingress {
-    description = "ssh access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "jenkins server security group"
-  }
-}
-
 resource "aws_instance" "jenkins" {
   ami                         = "ami-04b70fa74e45c3917"
   instance_type               = "t2.micro"
   key_name                    = "debian"
-  vpc_security_group_ids      = [aws_security_group.ec2_security_group.id]
-  associate_public_ip_address = false
-
   tags = {
     Name = "jenkins-instance"
   }
